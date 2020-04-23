6407c5d44cc7eab997bdc35813cb6081f0dedc60
Sagar <svanteddu@salesforce.com>
2020-04-23 12:12:45 -0400

    After solutions - needs changes

--- src/www/js/functional/array.test.js
+++ src/www/js/functional/array.test.js
@@ -41,9 +41,12 @@ describe('Array higher-order functions', () => {
   describe('#every', () => {
     it('implement the #every method', () => {
       // === Uncomment me and implement ===
-      // Array.prototype.every = function(pred) {
-      //   console.log(this) // access the array with `this`
-      // }
+      Array.prototype.every = function(pred) {
+        let result = true
+        this.forEach(el => {
+          if(!pred(el)) result = false
+        })
+      }
       expect([1, 2, 3].every(x => x > 0)).toEqual(true)
       expect([1, 2, 3].every(x => x > 2)).toEqual(false)
     })
@@ -62,14 +65,8 @@ describe('Array higher-order functions', () => {
 
   describe('#map', () => {
     it('rewrite using #map', () => {
-      const incrementAgeOfHumans = (humans) => {
-        const result = []
-        for (let i = 0; i < humans.length; i++) {
-          result.push({ ...humans[i], age: humans[i].age + 1 })
-        }
-        return result
-      }
-
+      //const incrementAgeOfHumans = (humans) => humans.map((human) )
+      
       expect(incrementAgeOfHumans(users)).toEqual([
         { id: 1, name: 'Andrew', age: 34, favoriteColor: 'blue' },
         { id: 2, name: 'Billy', age: 21, favoriteColor: 'red' },
@@ -81,12 +78,19 @@ describe('Array higher-order functions', () => {
   describe('#reduce', () => {
     it('rewrite using reduce', () => {
       const doubleSum = (vals) => {
-        let result = 0
-        for (let i = 0; i < vals.length; i++) {
-          result += vals[i] * 2
-        }
-        return result
+        return vals.reduce(
+          (acc, val) => acc + (val*2), 0
+        )
       }
+
+
+      // const doubleSum = (vals) => {
+      //   let result = 0
+      //   for (let i = 0; i < vals.length; i++) {
+      //     result += vals[i] * 2
+      //   }
+      //   return result
+      // }
 
       expect(doubleSum([1, 2, 3])).toEqual(2 + 4 + 6)
     })
@@ -94,13 +98,9 @@ describe('Array higher-order functions', () => {
     it('rewrite #flatten using reduce', () => {
       // TIP: Use Array#concat to join two arrays together immutably
       const flatten = (tuples) => {
-        const result = []
-        for (let i = 0; i < tuples.length; i++) {
-          for (let j = 0; j < tuples[i].length; j++) {
-            result.push(tuples[i][j])
-          }
-        }
-        return result
+        return tuples.reduce(
+          (acc, tuple) => acc.concat(tuple)
+        )
       }
 
       expect(flatten([[0, 1], [2, 3, 4], [5], []])).toEqual([0, 1, 2, 3, 4, 5])
@@ -108,12 +108,14 @@ describe('Array higher-order functions', () => {
 
     it('rewrite #groupByName using reduce', () => {
       const groupByName = (arr) => {
-        const groupings = {}
-        for (const val of arr) {
-          const currentVal = groupings[val] || 0
-          groupings[val] = currentVal + 1
-        }
-        return groupings
+        return arr.reduce(
+          (acc, x) => {
+            const currentVal = acc[val] || 0
+            acc[val] = currentVal + 1
+            return acc
+          },
+          {}
+        )
       }
       const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice']
       expect(groupByName(names)).toEqual({ Alice: 2, Bob: 1, Tiff: 1, Bruce: 1 })
